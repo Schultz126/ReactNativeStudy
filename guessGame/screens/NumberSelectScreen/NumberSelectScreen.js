706f1837aa49eb8a514d8b43d7c9afb80c681864
Schultz126 <rafael.drzevieski@gmail.com>
2026-03-19 09:10:33 -0300

    Button logic implemented

--- guessGame/screens/NumberSelectScreen/NumberSelectScreen.js
+++ guessGame/screens/NumberSelectScreen/NumberSelectScreen.js
@@ -1,6 +1,8 @@
 import { 
 	View,
 	TextInput, 
+	Alert,
+	Platform
 } from 'react-native';
 import selectStyles from './selectStyles';
 import MainButton from '../../components/mainButton';
@@ -8,6 +10,39 @@ import { useState } from 'react';
 
 const NumberSelectScreen = () => {
 	const [isFocused, setIsFocused] = useState(false);
+	const [number, setNumber] = useState('');
+
+	const message = (title, text) => {
+		if (Platform.OS === 'web') {
+        alert("Invalid Number");
+		} else {
+			Alert.alert( // Cria um pop-up para cofirmar a deleção do elemento selecionado
+				title, // Título do pop-up
+				text,
+				[
+					{ 
+						text: 'Ok', 
+						style: 'cancel' 
+					},
+				] // Vetor dos botões do alert
+			);
+		}
+	}
+
+	const handleConfirmButton = (testNumber) => {
+		if(testNumber === '') return
+		if(testNumber.includes('.') || testNumber.includes(',')) {
+			message(
+				"Invalid number",
+				"Only natural numbers are allowed"
+			)
+			setNumber('');
+		} else {
+			setNumber(testNumber);
+			console.log(number);
+			setNumber('');
+		}
+	}
 	return(
 		<View style={selectStyles.container}>
 			<TextInput 
@@ -15,10 +50,16 @@ const NumberSelectScreen = () => {
 				onFocus={() => setIsFocused(true)}
 				onBlur={() => setIsFocused(false)}
 				keyboardType='numeric'
-				maxLength={3}/>
+				maxLength={3}
+				value={number}
+				onChangeText={(value) => setNumber(value)}/>
 			<View style={selectStyles.buttonContainer}>
-				<MainButton text={'Reset'}/>
-				<MainButton text={'Confirm'}/>
+				<MainButton 
+				text={'Reset'}
+				action={() => {setNumber('')}}/>
+				<MainButton 
+				text={'Confirm'}
+				action={() => handleConfirmButton(number)}/>
 			</View>
 		</View>
 	)
